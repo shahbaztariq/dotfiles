392f3a9040df87c72fe6250b2da98a67e0311dff
Shahbaz Tariq <me@shahbaz.me.uk>
2014-07-01 10:45:54 +0100

    Removed helpers for now.

--- install.sh
+++ install.sh
@@ -5,9 +5,7 @@ echo "Installing DotFiles"
 DOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 rm -rf ~/.gitconfig
-rm -rf ~/.githelpers
 rm -rf ~/.globalignore
 
 ln -nfs $DOTDIR/.gitconfig ~/
-ln -nfs $DOTDIR/.githelpers ~/
 ln -nfs $DOTDIR/.globalignore ~/
