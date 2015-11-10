3f8578b2af13431187d9b3343784b069afe35365
Shahbaz Tariq <shahbaz@ssst.co>
2015-11-10 11:40:51 +0000

    missing functions

--- install.sh
+++ install.sh
@@ -4,18 +4,20 @@ echo "Installing DotFiles"
 
 DOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-rm -rf ~/.gitconfig
-rm -rf ~/.globalignore
+rm -rf ~/.aliases
 rm -rf ~/.bash_profile
 rm -rf ~/.bash_prompt
 rm -rf ~/.bashrc
-rm -rf ~/.aliases
+rm -rf ~/.functions
+rm -rf ~/.gitconfig
+rm -rf ~/.globalignore
 rm -rf ~/.inputrc
 
-ln -nfs $DOTDIR/.gitconfig ~/
-ln -nfs $DOTDIR/.globalignore ~/
+ln -nfs $DOTDIR/.aliases ~/
 ln -nfs $DOTDIR/.bash_profile ~/
 ln -nfs $DOTDIR/.bash_prompt ~/
-ln -nfs $DOTDIR/.aliases ~/
-ln -nfs $DOTDIR/.inputrc ~/
 ln -nfs $DOTDIR/.bashrc ~/
+ln -nfs $DOTDIR/.functions ~/
+ln -nfs $DOTDIR/.gitconfig ~/
+ln -nfs $DOTDIR/.globalignore ~/
+ln -nfs $DOTDIR/.inputrc ~/
