26cdbd2f108344176f4e3fd01d4283ba03fc5b5c
Shahbaz Tariq <shahbaz@ssst.co>
2015-11-10 11:15:59 +0000

    updates

--- install.sh
+++ install.sh
@@ -6,6 +6,14 @@ DOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 rm -rf ~/.gitconfig
 rm -rf ~/.globalignore
+rm -rf ~/.bash_profile
+rm -rf ~/.bash_prompt
+rm -rf ~/.aliases
+rm -rf ~/.inputrc
 
 ln -nfs $DOTDIR/.gitconfig ~/
 ln -nfs $DOTDIR/.globalignore ~/
+ln -nfs $DOTDIR/.bash_profile ~/
+ln -nfs $DOTDIR/.bash_prompt ~/
+ln -nfs $DOTDIR/.aliases ~/
+ln -nfs $DOTDIR/.inputrc ~/
